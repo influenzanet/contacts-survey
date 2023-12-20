09aeae7023bc90dc18a3925a56e51531d87e9ab1
Matteo Delfino <matteo.delfino@isi.it>
2023-12-20 16:48:54 +0100

    prettify LanguageHelpers

--- src/influenzanet-verdi-contact-extension/languages/languageHelpers.ts
+++ src/influenzanet-verdi-contact-extension/languages/languageHelpers.ts
@@ -3,8 +3,8 @@ import { Logger } from "case-editor-tools/logger/logger";
 import { Study } from "case-editor-tools/types/study";
 
 export interface Language {
-    languageId: string;
-    translations: any;
+  languageId: string;
+  translations: any;
 }
 
 type TranslateArray = [string, string][];
@@ -31,121 +31,123 @@ const altKey = "alt";
 const anyKey = "any";
 
 export class LanguageMap extends Map<string, string> {
-
-    constructor(text: TranslateArray) {
-
-        const textMap = new Map(text);
-        const key = textMap.get(idKey) || "default";
-        const reference = textMap.get(referenceKey);
-        if (textMap.get(anyKey) === undefined) {
-            const alt = textMap.get(altKey);
-            const t = LanguageHelpers.findTranslations(key, reference, alt);
-            t.map((i) => i && textMap.set(...i))
-        }
-
-        textMap.delete(idKey);
-        textMap.delete(altKey);
-
-        super(textMap.entries());
+  constructor(text: TranslateArray) {
+    const textMap = new Map(text);
+    const key = textMap.get(idKey) || "default";
+    const reference = textMap.get(referenceKey);
+    if (textMap.get(anyKey) === undefined) {
+      const alt = textMap.get(altKey);
+      const t = LanguageHelpers.findTranslations(key, reference, alt);
+      t.map((i) => i && textMap.set(...i));
     }
+
+    textMap.delete(idKey);
+    textMap.delete(altKey);
+
+    super(textMap.entries());
+  }
 }
 
 export class LanguageHelpers {
+  static languages: Map<string, any> = new Map();
+  static languagesId: Map<string, any> = new Map();
 
-    static languages: Map<string, any> = new Map();
-    static languagesId: Map<string, any> = new Map();
-
-    /**
+  /**
    * Missing keys by language => (key=>reference text)
    */
-    static missing: Map<string, TranslateMap> = new Map();
+  static missing: Map<string, TranslateMap> = new Map();
 
-    /**
+  /**
    *
    * @param languageId language id of this translation set
    * @param translations Translation set, list of named object. The key is the translation text id, each entry is a key value object with 2 entries, the 'en' text for reference text of the translation, the language code with the text to be used a the translated text in this language
    * @param source name of the source file (without .json extension) containing the loaded translation (to be used to generate missings)
    */
-    static addLanguage(languageId: string, translations: any, source?: string) {
-        console.log("registering translations for " + languageId, source);
-        if (!LanguageHelpers.languages.has(languageId))
-            LanguageHelpers.languages.set(languageId, {});
+  static addLanguage(languageId: string, translations: any, source?: string) {
+    console.log("registering translations for " + languageId, source);
+    if (!LanguageHelpers.languages.has(languageId))
+      LanguageHelpers.languages.set(languageId, {});
 
-        const translations_ = LanguageHelpers.languages.get(languageId);
-        translations = { ...translations_, ...translations };
-        LanguageHelpers.languages.set(languageId, translations);
-    }
+    const translations_ = LanguageHelpers.languages.get(languageId);
+    translations = { ...translations_, ...translations };
+    LanguageHelpers.languages.set(languageId, translations);
+  }
 
-    /**
+  /**
    * Declare a key is missing for a given language
    * @param key Missing key
    * @param language language id
    */
-    static setMissing(key: string, language: string, reference?: string) {
-        let m = LanguageHelpers.missing.get(language);
-        if (m == undefined) {
-            m = new Map<string, string>();
-            LanguageHelpers.missing.set(language, m);
-        }
-        m.set(key, typeof (reference) == "undefined" ? "_unknown_" : reference);
+  static setMissing(key: string, language: string, reference?: string) {
+    let m = LanguageHelpers.missing.get(language);
+    if (m == undefined) {
+      m = new Map<string, string>();
+      LanguageHelpers.missing.set(language, m);
     }
+    m.set(key, typeof reference == "undefined" ? "_unknown_" : reference);
+  }
 
-    static getStrings(key: string, reference?: string): string {
-        return "[ERROR] Translation missing";
-    }
+  static getStrings(key: string, reference?: string): string {
+    return "[ERROR] Translation missing";
+  }
 
-    /**
+  /**
    * Find translations for a given key id, if not exists try to use alt key if provided
    * @param id
    * @param reference
    * @param alt
    * @returns
    */
-    static findTranslations(id: string, reference?: string, alt?: string): TranslateArray {
-        const text: TranslateArray = [];
+  static findTranslations(
+    id: string,
+    reference?: string,
+    alt?: string
+  ): TranslateArray {
+    const text: TranslateArray = [];
 
-        LanguageHelpers.languages.forEach((translations, languageId) => {
-
-            // Find a translation in the current set
-            const get_trans = (key: string): [string, string] | undefined => {
-
-                if (!(key in translations)) {
-                    return undefined;
-                }
-
-                if (reference && translations[key][referenceKey] !== reference) {
-                    Logger.warn(`[WARNING] string ${key}[${languageId}] does not match survey definition!`);
-                    Logger.warn(`"${translations[key][referenceKey]}" !== "${reference}"\n`);
-                }
-
-                let trans = translations[key][languageId];
-                return [languageId, trans];
-            };
-
-            let trans;
-
-            trans = get_trans(id);
-            if (!trans && alt) {
-                trans = get_trans(alt);
-            }
-
-            if (trans) {
-                text.push(trans);
-            } else {
-                LanguageHelpers.setMissing(id, languageId, reference);
-                console.warn(`Missing ${id} for ${languageId}`);
-            }
-
-        });
-        return text;
-    }
-
-    static initLanguages(languages: Language[]) {
-        LanguageHelpers.languages = new Map();
-        for (const language of languages) {
-            LanguageHelpers.addLanguage(language.languageId, language.translations);
+    LanguageHelpers.languages.forEach((translations, languageId) => {
+      // Find a translation in the current set
+      const get_trans = (key: string): [string, string] | undefined => {
+        if (!(key in translations)) {
+          return undefined;
         }
+
+        if (reference && translations[key][referenceKey] !== reference) {
+          Logger.warn(
+            `[WARNING] string ${key}[${languageId}] does not match survey definition!`
+          );
+          Logger.warn(
+            `"${translations[key][referenceKey]}" !== "${reference}"\n`
+          );
+        }
+
+        let trans = translations[key][languageId];
+        return [languageId, trans];
+      };
+
+      let trans;
+
+      trans = get_trans(id);
+      if (!trans && alt) {
+        trans = get_trans(alt);
+      }
+
+      if (trans) {
+        text.push(trans);
+      } else {
+        LanguageHelpers.setMissing(id, languageId, reference);
+        console.warn(`Missing ${id} for ${languageId}`);
+      }
+    });
+    return text;
+  }
+
+  static initLanguages(languages: Language[]) {
+    LanguageHelpers.languages = new Map();
+    for (const language of languages) {
+      LanguageHelpers.addLanguage(language.languageId, language.translations);
     }
+  }
 }
 
 /**
@@ -156,33 +158,30 @@ export class LanguageHelpers {
  * @returns
  */
 export function _T(id: string, en?: string, alt?: string): LanguageMap {
+  const t: TranslateArray = [];
 
-    const t: TranslateArray = [];
+  if (alt) {
+    t.push([altKey, alt]);
+  }
 
-    if (alt) {
-        t.push([altKey, alt]);
-    }
+  if (en) {
+    t.push([referenceKey, en]);
+  }
 
-    if (en) {
-        t.push([referenceKey, en]);
-    }
+  const idEntry = {
+    [id]: {
+      [idKey]: id,
+    },
+  };
 
-    const idEntry = {
-        [id]: {
-            [idKey]: id
-        }
-    };
+  LanguageHelpers.languagesId.set(idKey, {
+    ...idEntry,
+    ...LanguageHelpers.languages.get(idKey),
+  });
 
-    LanguageHelpers.languagesId.set(idKey, { ...idEntry, ...LanguageHelpers.languages.get(idKey) });
-
-    return new LanguageMap([
-        [idKey, id],
-        ...t
-    ]);
+  return new LanguageMap([[idKey, id], ...t]);
 }
 
 export function _T_any(en: string): LanguageMap {
-    return new LanguageMap([
-        [anyKey, en]
-    ]);
+  return new LanguageMap([[anyKey, en]]);
 }
